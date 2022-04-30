01a9528440ba49a1e0644d65ada1094a12e04806
Semilore Idowu <semijacks27@gmail.com>
2022-04-30 15:43:07 +0100

    finished user authentication

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,14 +1,94 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  json,
+  ActionFunction,
+  useActionData,
+  LoaderFunction,
+  redirect,
+} from 'remix';
 import { Layout } from '../components/layout';
 import { FormField } from '../components/form-field';
-import React, { useState } from 'react';
+import {
+  validateEmail,
+  validatePassword,
+  validateName,
+} from '../utils/validators.server';
+import { login, register, getUser } from '../utils/auth.server';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return (await getUser(request)) ? redirect('/') : null;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const action = form.get('_action');
+  const email = form.get('email');
+  const password = form.get('password');
+  let firstName = form.get('firstName');
+  let lastName = form.get('lastName');
+
+  if (
+    typeof action !== 'string' ||
+    typeof email !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return json({ error: 'Invalid Form Data', form: action }, { status: 400 });
+  }
+
+  if (
+    action === 'register' &&
+    (typeof firstName !== 'string' || typeof lastName !== 'string')
+  ) {
+    return json({ error: 'Invalid Form Data', form: action }, { status: 400 });
+  }
+
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+    ...(action === 'register'
+      ? {
+          firstName: validateName((firstName as string) || ''),
+          lastName: validateName((lastName as string) || ''),
+        }
+      : {}),
+  };
+
+  if (Object.values(errors).some(Boolean)) {
+    return json(
+      {
+        errors,
+        fields: { email, password, firstName, lastName },
+        form: action,
+      },
+      { status: 400 }
+    );
+  }
+
+  switch (action) {
+    case 'login': {
+      return await login({ email, password });
+    }
+    case 'register': {
+      firstName = firstName as string;
+      lastName = lastName as string;
+      return await register({ email, password, firstName, lastName });
+    }
+    default:
+      return json({ error: 'Invalid Form Data' }, { status: 400 });
+  }
+};
 
 export default function Login() {
+  const actionData = useActionData();
+  const [formError, setFormError] = useState(actionData?.error || '');
+  const [errors, setErrors] = useState(actionData?.errors || {});
+  const firstLoad = useRef(true);
   const [action, setAction] = useState('login');
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: '',
+    email: actionData?.fields?.email || '',
+    password: actionData?.fields?.password || '',
+    firstName: actionData?.fields?.firstName || '',
+    lastName: actionData?.fields?.lastName || '',
   });
 
   const handleInputChange = (
@@ -20,6 +100,32 @@ export default function Login() {
       [field]: event.target.value,
     }));
   };
+
+  useEffect(() => {
+    //clear the form if we switch forms
+    if (!firstLoad.current) {
+      const newState = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+      };
+      setErrors(newState);
+      setFormError('');
+      setFormData(newState);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setFormError('');
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    //we don't want to reset errors on page load because we want to see them
+    firstLoad.current = false;
+  }, []);
 
   return (
     <Layout>
@@ -39,12 +145,16 @@ export default function Login() {
             : 'Sign Up To Get Started'}
         </p>
 
-        <form className='rounded-2xl bg-gray-200 p-6 w-96'>
+        <form method='post' className='rounded-2xl bg-gray-200 p-6 w-96'>
+          <div className='text-xs font-semibold text-center tracking-wide text-red-500 w-full'>
+            {formError}
+          </div>
           <FormField
             htmlFor='email'
             label='Email'
             value={formData.email}
             onChange={(e) => handleInputChange(e, 'email')}
+            error={errors?.email}
           />
 
           <FormField
@@ -53,6 +163,7 @@ export default function Login() {
             type='password'
             value={formData.password}
             onChange={(e) => handleInputChange(e, 'password')}
+            error={errors?.password}
           />
 
           {action !== 'login' ? (
@@ -62,6 +173,7 @@ export default function Login() {
                 label='First Name'
                 value={formData.firstName}
                 onChange={(e) => handleInputChange(e, 'firstName')}
+                error={errors?.firstName}
               />
 
               <FormField
@@ -69,6 +181,7 @@ export default function Login() {
                 label='Last Name'
                 value={formData.lastName}
                 onChange={(e) => handleInputChange(e, 'lastName')}
+                error={errors?.lastName}
               />
             </>
           ) : null}
